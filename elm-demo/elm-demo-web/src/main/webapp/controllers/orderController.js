b19386f1d4890428005ebe6abca5e364d13118f7
emas <e_maslov@yahoo.com>
2016-11-14 21:26:12 -0500

    new functionality to view orders

---
+++ elm-demo/elm-demo-web/src/main/webapp/controllers/orderController.js
@@ -0,0 +1,27 @@
+﻿angular.module("elmDemo")
+.constant("ordersUrl", "http://localhost:8080/elm-demo-web/api/orders")
+.controller("ordersCtrl", function ($scope, $http, ordersUrl) {
+
+    $http.get(ordersUrl)
+        .success(function (data) {
+            $scope.orders = data;
+        })
+        .error(function (error) {
+            $scope.error = error;
+        });
+
+    $scope.selectedOrder;
+
+    $scope.selectOrder = function (order) {
+        $scope.selectedOrder = order;
+    };
+
+    $scope.calcTotal = function (order) {
+        var total = 0;
+        for (var i = 0; i < order.products.length; i++) {
+            total +=
+                order.products[i].quantity * order.products[i].price;
+        }
+        return total;
+    }
+});
